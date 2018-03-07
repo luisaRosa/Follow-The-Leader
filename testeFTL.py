5e5ace9e03477c08d68c8929e451a26afaf07513
luisaRosa <luisasilveirarosa@gmail.com>
2018-03-07 18:07:34 -0500

    follow the leader whitout cplex complete
    still need to optimization

--- testeFTL.py
+++ testeFTL.py
@@ -1,8 +1,9 @@
 # coding=utf-8
-#Follow The Leader
+#Follow The Leader - FTL
 
 from random import *
 import random
+
 
 
 T = 10 # seta as rodadas do jogo
@@ -10,13 +11,14 @@ m = 7 # número de ações, ou especialistas
 l1= [(0.5,0), (0.1,1),(1,0)] # vetor de perdas
 
 
+# inicializa as listas com listas de tamanho m com zero's
 def inicialize_vector(vector):
 
 	for t in xrange(T):
 		vector.append([0] * m)
 	return vector
 
-
+# gera instancias randômicas para o FTL
 def generate_instances():
 
 	l = []
@@ -25,7 +27,6 @@ def generate_instances():
 	for i in xrange(T):
 		for j in xrange(m):
 			lost = uniform(0,1.1)
-
 			if lost <=1:
 				l[i][j] = lost
 			else:
@@ -33,31 +34,27 @@ def generate_instances():
 	return l
 
 
-
-def solve_problem():
+def follow_the_leader():
 	pt = [] # vetor de ações que são decididas com base nos valores das perdas anteriores
 	l = []
 	l = generate_instances()
-	print 'oi',l
+	print 'Instancia gerada:',l
 
-	
 	# inicializa o vetor de ações
-	for t in xrange(T):
-		pt.append([0] * m)
-	
+	pt = inicialize_vector(pt)	
 
 	# Escolhe as ações baseadas no vetor de perdas em t-1
 	for t in xrange(T):	
 
-		if t >=1:	
+		if t >=1:# verifica se já tem informações passadas para poder fazer a previsão	
 			sum_min_t, index_t= minimize(l[:t]) # escolhe a ação minima no tempo t, com base no vetor de perdas t-1
 			print "Vetor de perda t-1:",l[:t]
 			print " Soma minima em t =", sum_min_t 
 			pt[t][index_t] = 1 # seta o valor que minimiza a perda, baseado nas perdas ateriores
 
+	# escolhe uma determinada ação randômica para p1
 	rand = random.randint(0,m-1)
 	print rand
-
 	pt[0][rand] = 1
 
 	print "Vetor de ações:",pt
@@ -68,24 +65,23 @@ def solve_problem():
 		for i in xrange(m):
 			if pt[t][i] == 1:
 				total_lost+= l[t][i]
-
-
-
 	print  'A perda total é: ',total_lost
 
-
+# minimiza a a perda com base em t-1
 def minimize(lost_vector):
 
 	temp =[]
 	temp = [0]*m
 	sum_min = 1000
 	index_min = 0
-	
+
+	# faz a soma dos indices de m a partir do vetor de perdas em t-1
 	for i in xrange(len(lost_vector)):
 		for j in xrange(m):
 			aux = temp[j]
 			temp[j] =  aux+ lost_vector[i][j]
 
+	# identifica a soma mínima em t-1
 	for i in xrange(m):
 		if sum_min > temp[i]:
 			sum_min = temp[i]
@@ -94,62 +90,8 @@ def minimize(lost_vector):
 	return sum_min, index_min
 
 
-
-
-
-
-'''def minimize(lost_vector):
-	
-	# inicializa a soma e o índice mínimo
-	sum_min = 100 
-	index_min = 0
-	sum_total = []
-
-	# verifica qual é a menor perda anterior para poder realizar a ação
-
-	if len(lost_vector)>1:
-
-		for t in xrange(len(lost_vector)-1):
-
-			lv_t = lost_vector[t] 
-			lv_t1 = lost_vector[t+1]
-			for i in xrange(m-1):
-
-				#sum_total.apend(lost_vector[t][i]+lost_vector[])
-
-				"""if aux == lista_t:
-					if sum_min > lista_t[i]:
-						sum_min = lista_t[i]
-						index_min = i+1
-				else:"""
-				if (lv_t[i] + lv_t1[i])==(lv_t[i+1] + lv_t1[i+1]):
-					
-					if (sum_min+(lv_t[index_min] + lv_t1[index_min])) >  (lv_t[i] + lv_t1[i]):
-						s_p = sum_min
-						index_min_prev = index_min
-						index_min = i if index_min_prev!=i else i+1
-						sum_min= (lv_t[i] + lv_t1[i])+100					
-
-				elif (lv_t[i] + lv_t1[i]) >(lv_t[i+1] + lv_t1[i+1]):
-					if sum_min > lv_t[i+1] + lv_t1[i+1]:
-						sum_min += lv_t[i+1] + lv_t1[i+1]
-						index_min = i+1
-				else:
-					if sum_min >(lv_t[i] + lv_t1[i]):
-						sum_min += (lv_t[i] + lv_t1[i])
-						index_min = i
-	else:
-		
-		for t in xrange(m):
-			if sum_min > lost_vector[0][t]:
-				sum_min = lost_vector[0][t]+100
-				index_min = t
-				
-	return (float(sum_min - 100), index_min )
-'''
-
 if __name__ == '__main__':
-	solve_problem()
+	follow_the_leader()
 	#generate_instances()
 
 
